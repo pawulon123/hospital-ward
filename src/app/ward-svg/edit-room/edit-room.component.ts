06281da45fd16909732f8b9b63f6031b4ffd03a1
pawelchodzba123 <pawul0n@op.pl>
2021-12-21 21:22:11 +0100

    Ward template refactor move appBedEv - directive

--- src/app/ward-svg/edit-room/edit-room.component.ts
+++ src/app/ward-svg/edit-room/edit-room.component.ts
@@ -4,7 +4,7 @@ import { Bed } from '../../shared/models/bed';
 import { EditRoomService } from './../../core/services/edit-room.service';
 import { Component, OnInit } from '@angular/core';
 import { BedRotate } from './bed-rotate';
-import { cloneDeep, findById, logError } from '../../shared/useful/useful';
+import { findById } from '../../shared/useful/useful';
 @Component({
   selector: 'app-edit-room',
   templateUrl: './edit-room.component.html',
@@ -26,34 +26,24 @@ export class EditRoomComponent implements OnInit {
   }
   private passObjectEdit(objEditRoom: EditRoom): void {
     this.objectEdit = objEditRoom;
-    // this.getMarkedBed ? this.objectEdit.addOrUpdate({ id: this.getMarkedBed.id }) : logError(`this bed don't exist in room`);
-    // if (!this.objectEdit.roomNotModified) this.objectEdit.roomNotModified = cloneDeep(this.room);
-
   }
-  get getMarkedBed(): Bed  {
+  get markedBed(): Bed  {
    return this.objectEdit?.marked && this.room ? findById(this.room.beds, this.objectEdit?.marked): null;
   }
   rotate(): void {
     if (!this.objectEdit.marked) return;
     this.polygonBed = this.editRoomService.getOutputBed(this.objectEdit.marked).polygon;
-    const polygon = !this.polygonBed ? this.getMarkedBed?.polygon : this.polygonBed;
-    this.bedRotate.rotate({
-       id: this.getMarkedBed?.id,
-      polygon
-    });
-    console.log("rotate", this.bedRotate.points);
-
+    const polygon = !this.polygonBed ? this.markedBed?.polygon : this.polygonBed;
+    this.bedRotate.rotate({id: this.markedBed?.id, polygon});
     this.editRoomService.addOrUpdate(
       {
-        id: this.getMarkedBed?.id ,
+        id: this.markedBed?.id ,
         polygon: this.bedRotate.points
       }
     )
-    if (this.getMarkedBed) {
-      this.getMarkedBed.polygon = this.bedRotate.points;
-      this.editRoomService.modify(this.objectEdit);
-    }
 
+      this.markedBed.polygon = this.bedRotate.points;
+      this.editRoomService.modify(this.objectEdit);
 
   }
 
