7b09e79f085a1998c3bf4d5b3ff970a1abaea1ad
pawelCh <pchodzba@softres.pl>
2023-03-04 15:20:16 +0100

    startup changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { BaseWindowComponent } from './current-state/base-window/base-window.component';
 
 const routes: Routes = [
-  { path: 'current-stay', component: <any>BaseWindowComponent },
+  { path: '', component: <any>BaseWindowComponent },
 ];
 
 @NgModule({
